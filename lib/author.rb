58574ceaf946b9628b84ae1d946998b599fd77e7
Michelle Lam <shelly2.lam@gmail.com>
2019-06-26 18:09:20 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,6 @@
+class Author
+    attr_reader :name
+    def name= (name)
+        @name = name
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+class Song
+
+    attr_reader :title, :artist
+    def title=(song)
+        @title = song
+    end
+
+    def artist=(name)
+        @artist = name
+    end
+end
